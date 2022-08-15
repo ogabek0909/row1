38b6165d13b20fd5878996eaf21f934f3a302a27
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-08-15 06:57:20 +0500

    decoration

--- task2/lib/main.dart
+++ task2/lib/main.dart
@@ -1,43 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 //import 'package:flutter/services.dart';
-Widget inbox({double width =100, double height = 100, Color color =Colors.blue }){
-  return Container(
-    width: width,
-    height: height,
-    color: color,
-    margin: EdgeInsets.all(20),
+// Widget inbox({double width =100, double height = 100, Color color =Colors.blue }){
+//   return Container(
+//     width: width,
+//     height: height,
+//     color: color,
+//     margin: EdgeInsets.all(20),
 
-  );
-}
-Widget getbox({double width=150, double height=150, Color color=Colors.blueGrey}){
-  return Container(
-    color: color,
-    width: width,
-    height: height,
-    margin: EdgeInsets.all(5),
-    child: inbox(),
+//   );
+// }
+// Widget getbox({double width=150, double height=150, Color color=Colors.blueGrey}){
+//   return Container(
+//     color: color,
+//     width: width,
+//     height: height,
+//     margin: EdgeInsets.all(5),
+//     child: inbox(),
 
-  );
-}
+//   );
+// }
  void main(){
-  runApp(
-    Container(
-      
-      color: Colors.white,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [getbox(),getbox()],),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [getbox(width: 305,)],)
-          ],
-        ), 
-      ),
-    )
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    color: Colors.red,
+    home: Text('salom',textDirection: TextDirection.ltr,style: TextStyle(color: Colors.white,decoration:TextDecoration.none),),
+  ));
  }
