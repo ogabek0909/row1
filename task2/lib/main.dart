a69d98aa6c5747ec59322d1df719f663129d26d6
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-08-16 09:56:03 +0500

    dad

--- task2/lib/main.dart
+++ task2/lib/main.dart
@@ -1,29 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-//import 'package:flutter/services.dart';
-// Widget inbox({double width =100, double height = 100, Color color =Colors.blue }){
-//   return Container(
-//     width: width,
-//     height: height,
-//     color: color,
-//     margin: EdgeInsets.all(20),
+void main (){
+  runApp(
+    Container(
+      margin: EdgeInsets.fromLTRB(5, 30, 5, 10),
+      color: Colors.white,
+      child: MaterialApp(
+        
+        debugShowCheckedModeBanner: false,
+        home:Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('salom',textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.red,
+              decoration: TextDecoration.none
+            ),
+            ) ,
+            Container(
+              width: 350,
+              height: 700,
+              decoration: BoxDecoration(
+                color:const Color.fromARGB(255, 216, 37, 24),
+                image:const DecorationImage(
+                  image: NetworkImage('https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',scale: 12),
+                  fit: BoxFit.cover
+                ),
+                border:Border.all(
+                  color: Colors.red,
+                  width:8
+                ),
+                borderRadius: BorderRadius.circular(12)
+              ),
+            )
 
-//   );
-// }
-// Widget getbox({double width=150, double height=150, Color color=Colors.blueGrey}){
-//   return Container(
-//     color: color,
-//     width: width,
-//     height: height,
-//     margin: EdgeInsets.all(5),
-//     child: inbox(),
 
-//   );
-// }
- void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    color: Colors.red,
-    home: Text('salom',textDirection: TextDirection.ltr,style: TextStyle(color: Colors.white,decoration:TextDecoration.none),),
-  ));
- }
+          ],
+        )
+        
+      ),
+    )
+  );
+}
